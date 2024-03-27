af94a9db203fb51b7d00be8875040568c79b8e60
Lucas <loisabel@rd.com.br>
2024-03-27 10:16:56 -0300

    initial: commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { ThemeProvider } from "styled-components";
+import { DefaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={DefaultTheme}>
+      <h1> Hello World </h1>
+    </ThemeProvider>
+  );
+}
